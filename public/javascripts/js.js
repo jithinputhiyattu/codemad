361e5af71e11ca4d85cbad69de27a4f19d483f0f
Jishnu <jishnuvasanth@gmail.com>
2016-01-22 14:39:02 +0530

    Signup

---
+++ public/javascripts/js.js
@@ -0,0 +1,9 @@
+function overlay() {
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
+
+/*function overlay() {
+	el = document.getElementsByClassName("mdl-card__title");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}*/
