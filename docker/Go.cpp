a24f4199224902297e5d398364aa932bfa5344e4
JITHIN.P <jithinputhiyattu@gmail.com>
2016-02-20 11:00:15 +0530

    Go.cpp

--- docker/Go.cpp
+++ docker/Go.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
-#include <unistd>
+#include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <stdio.h>
@@ -20,35 +20,92 @@ void setProgram(char *source)
       }
       result[i] = source[i];
    }
-  strcat(result,".output"); 
   strcpy(temporary,result);
+  strcat(result,".output"); 
   strcat(temporary,".temporary");
  }
+
+void copyFromHost(char*id,int argc ,char* argv[])
+{
+
+}
+
+void compileAndRun(char*id,int argc ,char* argv[])
+{
+
+}
+void copyToHost(char*id,int argc ,char* argv[])
+{
+	
+}
+
+void extractId(char*line_i,char*id_o)
+{
+	int i,j;
+	for(i=0;i<strlen(line_i);i++)
+	{
+      if('@'==line_i[i])
+      {
+      	i++;
+      	for(j=0;i<strlen(line_i)&&':'!=line_i[i];i++)
+      	{
+          id_o[j++] = line_i[i];
+      	}
+      	id_o[j++] ='\0';
+      	break;
+      }
+	}
+}
+
 /*
   command line args 
   0 : program name
   1 : C/C++ file name
   2 : test cases
 */
-int main(int argv,char *argv[])
+int main(int argc,char *argv[])
 {
-  if(argv<2) return 0;
+  if(argc<2) return 0;
   char *cmd = new char[250];
   char *tmp = new char[250];
   setProgram(argv[1]);
-  strcpy(cmd,"docker run -it --rm -m 32m ubuntu14 > ");
-  strcat(cmd,temporary);
-  cout<<"executing : "<<cmd<<endl;
-  system(cmd);
+  remove(temporary);
   int child_id =0;
-  if(child_id=fork())
+  if((child_id=fork()))
   {
+    sleep(1);
+    FILE *fin=fopen(temporary,"r");
+    if(fin)
+    {
+        char *line = new char[500]; 
+        char *id = new char[100];
+        fscanf(fin," %[^\n]",line);
+        fclose(fin);
+        extractId(line,id);
+        cout<<endl<<id<<endl;
+        //copy file to the continer
+        copyFromHost(id,argc,argv);
+        compileAndRun(id,argc,argv);
+        copyToHost(id,argc,argv);
 
+        //clean the memory and delete the continer...
 
+        delete line;
+        delete id;
+    }
+    else
+    {
+    	cout<<"error msg : "<<"error in file operation.."<<endl;
+    }
+    wait(NULL);
   }
   else
   {
   	//child process
+    strcpy(cmd,"docker run -it ubuntu14 >> ");
+    strcat(cmd,temporary);
+    cout<<"executing : "<<cmd<<endl;
+    system(cmd);
   }
   delete cmd;
   delete tmp;
